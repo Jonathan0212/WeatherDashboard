daa95254760a9b89d5bb0067ffaa3a7771bdce37
Your Name <Vaaax@Jon-PC>
2021-12-27 23:59:42 -0600

    still working on JS, not functioning at the moment

--- Develop/script.js
+++ Develop/script.js
@@ -16,4 +16,59 @@ function find(c){
 
         }
     }
+    return 1;
+}
+
+var APIKey="10888b61aea738dbd51f14fba56c4df3";
+
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+
+function currentWeather(city){
+    var queryURl="https://api.openweathermap.org/data/2.5/weather?q=" + city +"&APPID=" + APIKey;
+    $.ajax({
+        url:queryURl,
+        method:"GET",
+    }).then(function(response){
+        console.log(response);
+        var weathericon = response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        var date = new Date(response.dt*1000).toLocaleDateString();
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
+
+        $(currentHumidity).html(response.main.humidity+"%");
+
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+
+        currentUvindex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("cityname"));
+            console.log(sCity);
+            if(sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname", JSON.stringify(sCity));
+                addToList(city);
+            }
+            else {
+                if(find(city)>0){
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
+     });
 }
